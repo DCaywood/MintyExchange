c24303920e661650632126fe967a7b69f5a27420
DCaywood <99561143+DCaywood@users.noreply.github.com>
2022-02-12 21:13:04 -0800

    Update algonode.js

--- src/services/algonode.js
+++ src/services/algonode.js
@@ -1,9 +1,9 @@
 import algosdk from 'algosdk';
 
-//const token = "2fd6f930612606e3346fc0291621b50d58833c2a02aadac75d721d7ce057147e";
+
 const token = "";
 const server = "https://algoexplorerapi.io/";
-//const port = "8112";
+
 const port = "";
 
 // algod client
